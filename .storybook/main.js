4c422225c451d7df90f394c5776e0d89fcb3134b
kimsyy <kimse7530@daum.net>
2023-05-14 13:11:48 +0900

    타입스크립트 관련 설정 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,6 +12,16 @@ const config = {
   },
   docs: {
     autodocs: "tag"
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: prop =>
+        (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true)
+    }
   }
 };
 export default config;
